c3d17269145d3c6d5945bfe815f74b8c66e78c9b
h5i3h <h5i3h@h5i3h-VirtualBox.(none)>
2016-06-10 02:30:41 +0800

    Add file operation write

--- rs232_master/rs232_master.c
+++ rs232_master/rs232_master.c
@@ -23,6 +23,7 @@ static void __exit rs232_master_final(void);
 /* File operations */
 static int rs232_master_open(struct inode *inode, struct file *filp);
 static int rs232_master_close(struct inode *inode, struct file *filp);
+static ssize_t rs232_master_write(struct file* filp, const char __user *buff, size_t data_size, loff_t* offp);
 
 static int open_connection(unsigned short port);
 static int close_connection(void);
@@ -32,7 +33,8 @@ static ssize_t send(struct socket* client_socket, char* buf, size_t size);
 static struct file_operations fops = {
 	.owner = THIS_MODULE,
 	.open = rs232_master_open,
-	.release = rs232_master_close
+	.release = rs232_master_close,
+	.write = rs232_master_write,
 };
 
 /* Some device variable */
@@ -40,24 +42,25 @@ static struct class* dev_class;
 static dev_t dev_no;
 static struct cdev cdev;
 
-static char* kernel_data; /* Allocate by kmalloc, free by kfree */
-static struct socket* client_socket; /* Server side kernel socket */
-static struct socket* server_socket; /* Client side kernel socket */
+static struct socket* client_socket; /* Clinet side kernel socket */
+static struct socket* server_socket; /* Server side kernel socket */
 
+static char* kernel_buff; /* Buffer in kernel space which is allocate by kmalloc and free by kfree */
 static size_t device_ref_count; /* Device reference count, record how many time this device been opened */
-static size_t data_len;
+static size_t datalen_in_kern_buf;
 static size_t unflush_index;
 
 DEFINE_SEMAPHORE(device_mutex);
 
-/** Create character device follow this procedure:
- * 	Register device number by alloc_chrdev_region
- * 	Initailize cdev object and set its file operations by cdev_init
- * 	Add this device to kernel by cdev_add
+/** 
+ * Create character device follow this procedure:
+ * Register device number by alloc_chrdev_region
+ * Initailize cdev object and set its file operations by cdev_init
+ * Add this device to kernel by cdev_add
  *
- *  Device file(at /dev/FILE_NAME) can be create as follow:
- *  Add new device class by class_create
- *  Create device file with the device number and file name by device_create
+ * Device file(at /dev/FILE_NAME) can be create as follow:
+ * Add new device class by class_create
+ * Create device file with the device number and file name by device_create
  */
 static int __init rs232_master_init(void) {
 	int ret;
@@ -88,11 +91,12 @@ static int __init rs232_master_init(void) {
 	
 	/* Since our deivce will have read or write file operations, 
 	 * we need memory allocation for the device which is done as follow */
-	if((kernel_data = kmalloc(65535, GFP_KERNEL)) == NULL) {
+	if((kernel_buff = kmalloc(65535, GFP_KERNEL)) == NULL) {
 		printk(KERN_ERR "[%s] kmalloc fail\n", DEVICE_NAME);
 		ret = -ENOMEM;
 		goto kmalloc_fail;
 	}
+	device_ref_count = 0;
 	printk(KERN_INFO "[%s] Initailization done\n", DEVICE_NAME);
 
 	return 0;
@@ -112,7 +116,7 @@ alloc_chrdev_region_fail:
 }
 
 static void __exit rs232_master_final(void) {
-	kfree(kernel_data);
+	kfree(kernel_buff);
 	cdev_del(&cdev);
 	device_destroy(dev_class, dev_no);
 	class_destroy(dev_class);
@@ -133,15 +137,21 @@ static int rs232_master_open(struct inode* inode, struct file* filp) {
 	}
 	/* If this is first time open this device, initialize some needed variable */
 	if(device_ref_count == 0) {
-		data_len = 0;
+		datalen_in_kern_buf = 0;
 		unflush_index = 0;
 		/* TODO open the connection with slave devece */
+		if((ret = open_connection(9999)) < 0) {
+			printk(KERN_ERR "[%s] open_connection fail, reutrn %d.\n", DEVICE_NAME, ret);
+			goto open_connection_fail;
+		}
 	}
 	device_ref_count++;
 	printk(KERN_INFO "[%s] open success\n", DEVICE_NAME);
 	up(&device_mutex);
 	return 0;
 
+open_connection_fail:
+	up(&device_mutex);
 down_interruptible_fail:
 	return ret;
 }
@@ -170,11 +180,12 @@ down_interruptible_fail:
 	return ret;
 }
 
-/** Open connection with slave device through kernel socket follow this procedure:
- *  1. Create server kernel socket
- *  2. Bind server socket to specific port
- *  3. Server socket listen to that port
- *  4. Create client socket and wait fot its connection
+/** 
+ * Open connection with slave device through kernel socket follow this procedure:
+ * 1. Create server kernel socket
+ * 2. Bind server socket to specific port
+ * 3. Server socket listen to that port
+ * 4. Create client socket and wait fot its connection
  */
 static int open_connection(unsigned short port) {
 	int ret;
@@ -223,6 +234,71 @@ kernel_setsockopt_fail:
 sock_create_kern_fail:
 	return ret;
 }
+
+/** 
+ * Copy the data in user space to kernel buffer which can be read by device, then send the data
+ * in kernel buffer to slave device with the following step:
+ * 1. Check whether there are data in kernel buffer and not send to slave device yet, if so, send it.
+ * 2. Copy the data in user space buffer to kernel space buffer and constantly check whether kernel buffer is full.
+ * If it's full, send the data in kernal buffer to slave device and flush kernel buffer.
+ * 3. Repead step 2 until all the data in user space has been copy to kernal space buffer.
+ */
+static ssize_t rs232_master_write(struct file* filp, const char __user *buff, size_t data_size, loff_t* offp) {
+	int ret;
+	size_t per_copy_len;
+	size_t copied_len = 0;
+	size_t remain_len = data_size;
+	if(down_interruptible(&device_mutex)) {
+		ret = -ERESTARTSYS;
+		printk(KERN_ERR "[%s] In rs232_master_write down_interruptible fail.\n", DEVICE_NAME);
+		goto down_interruptible_fail;
+	}
+	/* Check whether there is unsend data in kernel buffer, if yes, then send it and flush the buffer */
+	while(datalen_in_kern_buf > 0 && unflush_index > 0) {
+		if((ret = send(client_socket, &kernel_buff[unflush_index], datalen_in_kern_buf)) < 0) {
+			printk(KERN_ERR "[%s] send fail, return %d.\n", DEVICE_NAME, ret);
+			goto send_fail;
+		}
+		datalen_in_kern_buf -= ret;
+		unflush_index += ret;
+	}
+	unflush_index = 0;
+	while(remain_len > 0) {
+		per_copy_len = 65535 - datalen_in_kern_buf;
+		if(per_copy_len > remain_len)
+			per_copy_len = remain_len;
+		if((copy_from_user(&kernel_buff[datalen_in_kern_buf], &buff[copied_len], per_copy_len)) < 0) {
+			ret = -EFAULT;
+			printk(KERN_ERR "[%s] copy_from_user fail.\n", DEVICE_NAME);
+			goto copy_from_user_fail;
+		}
+		remain_len -= per_copy_len;
+		datalen_in_kern_buf += per_copy_len;
+		unflush_index += per_copy_len;
+		copied_len += per_copy_len;
+		/* If kernel buffer is full, send all the data to slave device and flush it */
+		if(65535 == datalen_in_kern_buf) {
+			while(datalen_in_kern_buf > 0) {
+				if((ret = send(client_socket, &kernel_buff[unflush_index], datalen_in_kern_buf)) < 0) {
+					printk(KERN_ERR "[%s] send fail, return %d.\n", DEVICE_NAME, ret);
+					goto send_fail;
+				}
+				datalen_in_kern_buf -= ret;
+				unflush_index += ret;
+			}
+			unflush_index = 0;
+		}
+	}
+	up(&device_mutex);
+	return (long)copied_len;
+
+copy_from_user_fail:
+send_fail:
+	up(&device_mutex);
+down_interruptible_fail:
+	return ret;
+}
+
 static int close_connection(void) {
 	sock_release(server_socket);
 	sock_release(client_socket);
