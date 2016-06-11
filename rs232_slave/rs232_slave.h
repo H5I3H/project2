7ba67f384e81fe062eee68ccf544a43f92d171f3
Xwei <tsai.dennis12@gmail.com>
2016-06-11 12:45:32 +0800

    modify test_slave.c

--- rs232_slave/rs232_slave.h
+++ rs232_slave/rs232_slave.h
@@ -7,6 +7,19 @@
 
 #define RS232_SLAVE_CONNECT_PORT 9999
 
+#define ENCODE_IP(ip, ip_arr)         \
+	ip = (ip_arr[3] |                 \
+	ip_arr[2] << 8  |                 \
+	ip_arr[1] << 16 |                 \
+	ip_arr[0] << 24)
+#define DECODE_IP(ip, ip_arr)           \
+	do {                                \
+		ip_arr[3] = ip & 0xFF;          \
+		ip_arr[2] = (ip >> 8) & 0xFF;   \
+		ip_arr[1] = (ip >> 16) & 0xFF;  \
+		ip_arr[0] = (ip >> 24) & 0xFF;  \
+	} while (0)
+
 #define IOCTL_MAGIC	0x90  //  choose one number after consulting ioctl-number.txt 's'
 
 #define IOCTL_OPENCONN	_IOW(IOCTL_MAGIC, 0, unsigned int)
--- test_slave.c
+++ test_slave.c
@@ -20,110 +20,109 @@ int fdin, fdout;
 
 int main(int argc, char *argv[])
 {
-        unsigned int ip, ip_arr[4];
-        size_t len;
+    unsigned int ip, ip_arr[4];
+    size_t len;
         
-        struct timeval start, end;
-        struct stat stat;
+    struct timeval start, end;
+    struct stat stat;
         
-        if (argc < 4) {
-                fprintf(stderr, "usage: %s [output_file] [method] [IP]\n", argv[0]);
-                exit(1);
-        }
-        if ((fdin = open("/dev/rs232_slave", O_RDONLY)) < 0) {
-                perror("open");
-                exit(1);
-        }
-		if(gettimeofday(&start, NULL) < 0) {
-			perror("gettimeofday");
-			exit(1);
-		}
-        if ((fdout = open(argv[1], O_RDWR | O_TRUNC | O_CREAT, 0644)) < 0) {
-                perror("open");
-                exit(1);
-        }
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s <output_file> <method> <IP>\n", argv[0]);
+        exit(1);
+    }
+    if ((fdin = open("/dev/rs232_slave", O_RDONLY)) < 0) {
+        perror("open device fail");
+        exit(1);
+    }
+    if(gettimeofday(&start, NULL) < 0) {
+        perror("gettimeofday to start fail");
+        exit(1);
+    }
+    if ((fdout = open(argv[1], O_RDWR | O_TRUNC | O_CREAT, 0644)) < 0) {
+        perror("open fail");
+        exit(1);
+    }
 
-        if (sscanf(argv[3], "%u.%u.%u.%u",
-                   &ip_arr[0], &ip_arr[1], &ip_arr[2], &ip_arr[3]) != 4) {
-                fprintf(stderr, "invalid IP\n");
-                exit(1);
-        }
-        ENCODE_IP(ip, ip_arr);
-        if (ioctl(fdin, RS232_SLAVE_IOCOPENCONN, &ip) < 0) {
-                perror("ioctl");
-                exit(1);
-        }
+    if (sscanf(argv[3], "%u.%u.%u.%u",
+        &ip_arr[0], &ip_arr[1], &ip_arr[2], &ip_arr[3]) != 4) {
+        fprintf(stderr, "invalid IP\n");
+        exit(1);
+    }
+    ENCODE_IP(ip, ip_arr);
+    if (ioctl(fdin, IOCTL_OPENCONN, &ip) < 0) {
+        perror("ioctl open connect fail");
+        exit(1);
+    }
         
-        len = strlen(argv[2]);
-        if (strncmp("fcntl", argv[2], len) == 0) {
-                do_fcntl();
-        } else if (strncmp("mmap", argv[2], len) == 0) {
-                do_mmap();
-        } else {
-                fprintf(stderr, "invalid method\n");
-                exit(1);
-        }
+    len = strlen(argv[2]);
+    if (strncmp("fcntl", argv[2], len) == 0) {
+        do_fcntl();
+    } else if (strncmp("mmap", argv[2], len) == 0) {
+        do_mmap();
+    } else {
+        fprintf(stderr, "invalid method\n");
+        exit(1);
+    }
         
-        if (ioctl(fdin, RS232_SLAVE_IOCCLOSECONN) < 0) {
-                perror("ioctl");
-                exit(1);
-        }
+    if (ioctl(fdin, IOCTL_CLOSECONN) < 0) {
+        perror("ioctl close connect fail");
+        exit(1);
+    }
         
-        if (close(fdin) < 0) {
-                perror("close");
-                exit(1);
+    if (close(fdin) < 0) {
+        perror("close");
+        exit(1);
+    }
+    if (fstat(fdout, &stat) < 0) {
+        perror("fstat fail");
+        exit(1);
         }
-        if (fstat(fdout, &stat) < 0) {
-                perror("fstat");
-                exit(1);
-        }
-        if (close(fdout) < 0) {
-                perror("close");
-                exit(1);
-        }
+    if (close(fdout) < 0) {
+        perror("close");
+        exit(1);
+    }
 
-        if (gettimeofday(&end, NULL) < 0) {
-                perror("gettimeofday");
-                exit(1);
-        }
+    if (gettimeofday(&end, NULL) < 0) {
+        perror("gettimeofday to end fail");
+        exit(1);
+    }
         
-        if (end.tv_usec < start.tv_usec) {
-                end.tv_sec -= 1;
-                end.tv_usec += 1000000;
-        }
-        printf("Transmission time: %lf ms, File size: %lu bytes\n",
-               ((end.tv_sec - start.tv_sec) * 1000 +
-                ((double)(end.tv_usec - start.tv_usec) / 1000)),
-               (size_t)(stat.st_size));
+    if (end.tv_usec < start.tv_usec) {
+        end.tv_sec -= 1;
+        end.tv_usec += 1000000;
+    }
+    printf("Transmission time: %lf ms, File size: %lu bytes\n",
+        ((end.tv_sec - start.tv_sec) * 1000 +
+        ((double)(end.tv_usec - start.tv_usec) / 1000)),
+        (size_t)(stat.st_size));
 
-        return 0;
+    return 0;
 }
 
 static void do_fcntl(void)
 {
-        int ret;
-
-        char buf[RS232_SLAVE_DATA_SIZE];
+    int ret;
+    char buf[ RS232_SLAVE_DATA_SIZE ];
         
-        while ((ret = read(fdin, buf, sizeof(buf))) > 0) {
-                if (write(fdout, buf, ret) < 0) {
-                        perror("write");
-                        exit(1);
-                }
+    while ((ret = read(fdin, buf, sizeof(buf))) > 0) {
+        if (write(fdout, buf, ret) < 0) {
+            perror("write fail");
+            exit(1);
         }
-        if (ret < 0) {
-                perror("read");
-                exit(1);
-        }
+    }
+    if (ret < 0) {
+        perror("read fail");
+        exit(1);
+    }
 }
 static void do_mmap(void)
 {
-        long ret;
+    long ret;
         
-        off_t off;
-        size_t size;
-        void *bufin, *bufout;
-		void *temp;
+    off_t off;
+    size_t size;
+    void *bufin, *bufout;
+    void *temp;
 
         if ((bufin = mmap(0,
                           RS232_SLAVE_DATA_SIZE,
@@ -137,7 +136,7 @@ static void do_mmap(void)
 
         size = RS232_SLAVE_DATA_SIZE;
         off = 0;
-		ret = ioctl(fdin, RS232_SLAVE_IOCRECEIVEFROMMASTER, &size);
+		ret = ioctl(fdin, IOCTL_RECEIVEFROMMASTER, &size);
 		if(ftruncate(fdout, off + ret) < 0) {
 			perror("lseek");
 			exit(1);
@@ -148,7 +147,7 @@ static void do_mmap(void)
 		}
 		memcpy(bufout, bufin, ret);
 		off += ret;
-        while ((ret = ioctl(fdin, RS232_SLAVE_IOCRECEIVEFROMMASTER, &size)) > 0) {
+        while ((ret = ioctl(fdin, IOCTL_RECEIVEFROMMASTER, &size)) > 0) {
                 if (ftruncate(fdout, off + ret) < 0) {
                         perror("lseek");
                         exit(1);
