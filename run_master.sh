5c8b54c06f963d99886809ca9ec6031780a4fa98
h5i3h <h5i3h@h5i3h-VirtualBox.(none)>
2016-06-14 01:43:35 +0800

    Modified run_master.sh and run_slave.sh

--- run_master.sh
+++ run_master.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
-if [ $# -eq 0 ] || [ $# -ge 2 ]; then
-	echo "Usage: ${0} <input file>"
+if [ $# -eq 0 ] || [ $# -ge 3 ]; then
+	echo "Usage: ${0} <input file> <method>"
 else
-	echo "---Both master and slave use mmap---"
+	echo "Master use ${2}"
 	for count in {1..10}
 	do
-		./master ${1} mmap
-	done
-
-	echo "---Both master and slave use fcntl---"
-	for count in {1..10}
-	do
-		./master ${1} fcntl
+		./master ${1} ${2}
 	done
 fi
--- run_slave.sh
+++ run_slave.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env bash
-if [ $# -eq 0 ] || [ $# -ge 3 ]; then
-	echo "Usage: ${0} <output file> <ip>"
+if [ $# -eq 0 ] || [ $# -ge 4 ]; then
+	echo "Usage: ${0} <output file> <method> <ip>"
 else
-	echo "Both slave and master use mmap"
+	echo "Slave use ${2}"
 	temp=0
 	for count in {1..10}
 	do
-		temp=${temp}+$(./slave ${1} mmap ${2} | awk '{print $3}')
+		useless=$(./slave ${1} ${2} ${3})
+		echo ${useless}
+		temp=${temp}+$(echo ${useless} | awk '{print $3}')
+		#temp=${temp}+$(./slave ${1} ${2} ${3} | awk '{print $3}')
 	done
 	total=$(echo ${temp} | bc -l)
 	mmap_average=$(echo ${total}/10 | bc -l)
-	echo "mmap average time: ${mmap_average}"
-
-	echo "Both slave and master use fcntl"
-	temp=0
-	for count in {1..10}
-	do
-		temp=${temp}+$(./slave ${1} fcntl ${2} | awk '{print $3}')
-	done
-	total=$(echo ${temp} | bc -l)
-	fcntl_average=$(echo ${total}/10 | bc -l)
-	echo "fcntl average time: ${fcntl_average}"
+	echo "Average time: ${mmap_average} ms"
 fi
